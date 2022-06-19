dd22623f44e50312ac56e9a08b156d2c38fcb1bc
vgmda <vasilegancin@gmail.com>
2022-06-19 16:32:08 +0100

    Bootstrap form

--- RazorPagesUI/Models/AddressModel.cs
+++ RazorPagesUI/Models/AddressModel.cs
@@ -3,10 +3,11 @@ namespace RazorPagesUI.Models;
 
 public class AddressModel
 {
-    public string StreetAddress { get; set; }
+    public string StreetAddress1 { get; set; }
+    public string StreetAddress2 { get; set; }
     public string City { get; set; }
-    public string State { get; set; }
-    public string ZipCode { get; set; }
+    public string County { get; set; }
+    public string Postcode { get; set; }
 
 }
 
--- RazorPagesUI/Pages/Forms/AddAddress.cshtml.cs
+++ RazorPagesUI/Pages/Forms/AddAddress.cshtml.cs
@@ -21,10 +21,17 @@ public class AddAddressModel : PageModel
 
     public IActionResult OnPost()
     {
+        /*
+        
+        ModelState.IsValid will basically tell you if there is any issues with your data posted to the server -
+        based on the data annotations added to the properties of your model.
+
         if (ModelState.IsValid == false)
         {
             return Page();
         }
+
+        */
 
         return RedirectToPage("/Index", new { Address.City });
     }
